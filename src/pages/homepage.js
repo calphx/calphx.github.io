fc689bf3bdfa8b969736554df1a1418467dce70c
caroll675 <zhiqinchen@berkeley.edu>
2022-05-26 16:46:33 -0700

    0

---
+++ src/pages/homepage.js
@@ -0,0 +1,11 @@
+import * as React from "react"
+import { Link } from "gatsby"
+import Layout from "../components/layout"
+
+const HomePage = () => {
+    return (
+        <Layout pageHeading= 'Home' pageTitle = 'Hellaur'></Layout>
+    )
+}
+
+export default HomePage
